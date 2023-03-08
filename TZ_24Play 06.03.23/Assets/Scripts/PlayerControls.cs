b42bc684efc73ec8acdcbc8ec41799ab32380b62
Max Yevtushok <127140228+MaksymYevtushok@users.noreply.github.com>
2023-03-08 14:33:05 +0200

    mobile control

--- TZ_24Play 06.03.23/Assets/Scripts/PlayerControls.cs
+++ TZ_24Play 06.03.23/Assets/Scripts/PlayerControls.cs
@@ -4,39 +4,58 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-
-    [SerializeField] private DattaTransmitter dattaTransmitter;
-
     [SerializeField] private float forwardMovementSpeed;
-    [SerializeField] private float horizontalMovementSpeed;
-    [SerializeField] private float horizontalLimitValue;
-    private float newPositionX;
-    public Touch touch;
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    private float minX, maxX;
     public  SpawnManager spawnManager;
 
-    void FixedUpdate() 
+    void Start()
     {
-        SetForwardMovement();
-        SetHorizontalMovement(); 
+        // Set the minimum and maximum X positions for the object
+        minX = -2f;
+        maxX = 2f;
+    }
+    
+    void OnMouseDown()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+            offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, screenPoint.z));
+        }
     }
 
-    private void SetForwardMovement()
+    void OnMouseDrag()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector3 cursorScreenPoint = new Vector3(touch.position.x, touch.position.y, screenPoint.z);
+            Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorScreenPoint) + offset;
+            float clampedX = Mathf.Clamp(cursorPosition.x, minX, maxX);
+            transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+        }
+    }
+
+     void FixedUpdate() 
+    {
+        SetForwardMovement();
+    }
+
+      private void SetForwardMovement()
     {
         transform.Translate(Vector3.forward * forwardMovementSpeed * Time.fixedDeltaTime);
     }
 
-    private void SetHorizontalMovement()
-    {
-        newPositionX = transform.position.x + dattaTransmitter.GetHorizontalValue() * horizontalMovementSpeed * Time.fixedDeltaTime;
-        newPositionX = Mathf.Clamp(newPositionX, -horizontalLimitValue, horizontalLimitValue);
-        transform.position = new Vector3(newPositionX, transform.position.y, transform.position.z);
-    }
-
-    private void OnTriggerEnter(Collider other) 
+     private void OnTriggerEnter(Collider other) 
     {
         if(other.tag == "SpawnTrigger")
         {
            spawnManager.SpawnTriggerEntered();
         }
     }
+
+
 }
